e6972c88262508284a1fa1244a56f514b4ec0bd1
Ellie OConnor <ellieoconnor3114@gmail.com>
2025-01-09 19:49:42 -0500

    Refactor Jest setup and optimize testing configuration.
    Updated Jest setup to use `setupZoneTestEnv` for better compatibility with Angular's zone handling. Removed unused test and streamlined build steps in GitHub Actions. Enhanced Jest configuration by ignoring unnecessary files, refining coverage collection, and adding support for CSS/SCSS handling.

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,46 @@
 module.exports = {
 	preset: 'jest-preset-angular',
+	testEnvironment: 'jsdom',
 	collectCoverage: true,
 	coverageDirectory: 'coverage',
-	collectCoverageFrom: ['src/**/*.ts'],
+	collectCoverageFrom: [
+		'src/**/*.ts',
+		'!src/main.ts',
+		'!src/app.config.ts',
+	],
+	coveragePathIgnorePatterns: [
+		'/node_modules/',
+		'src/main.ts',
+		'src/app.config.ts',
+	],
 	coverageThreshold: {
 		global: {
 			branches: 80,
 			functions: 80,
 			lines: 80,
-			statements: 80
-		}
+			statements: 80,
+		},
 	},
 	setupFilesAfterEnv: ['<rootDir>/src/setup.jest.ts'],
-	testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
-	globals: {
-		'ts-jest': {
-			tsconfig: '<rootDir>/tsconfig.spec.json',
-			stringifyContentPathRegex: '\\.html$'
-		}
+	testPathIgnorePatterns: [
+		'<rootDir>/node_modules/',
+		'<rootDir>/dist/',
+		'<rootDir>/src/main.ts',
+		'<rootDir>/webpack.config.js',
+		'<rootDir>/src/app.config.ts',
+	],
+	transform: {
+		'^.+\\.(ts|mjs|js|html)$': [
+			'jest-preset-angular',
+			{
+				tsconfig: '<rootDir>/tsconfig.spec.json',
+				stringifyContentPathRegex: '\\.html$',
+			},
+		],
 	},
 	moduleNameMapper: {
-		'^src/(.*)$': '<rootDir>/src/$1'
-	}
+		'\\.(css|scss)$': 'identity-obj-proxy',
+		'^src/(.*)$': '<rootDir>/src/$1',
+	},
+	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
