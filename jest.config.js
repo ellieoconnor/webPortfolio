9051141dc98dd2e47114bcbfe400e3776bfc9034
Ellie OConnor <ellieoconnor3114@gmail.com>
2025-01-09 19:53:15 -0500

    Normalize formatting and clean up configuration files

--- jest.config.js
+++ jest.config.js
@@ -3,23 +3,15 @@ module.exports = {
 	testEnvironment: 'jsdom',
 	collectCoverage: true,
 	coverageDirectory: 'coverage',
-	collectCoverageFrom: [
-		'src/**/*.ts',
-		'!src/main.ts',
-		'!src/app.config.ts',
-	],
-	coveragePathIgnorePatterns: [
-		'/node_modules/',
-		'src/main.ts',
-		'src/app.config.ts',
-	],
+	collectCoverageFrom: ['src/**/*.ts', '!src/main.ts', '!src/app.config.ts'],
+	coveragePathIgnorePatterns: ['/node_modules/', 'src/main.ts', 'src/app.config.ts'],
 	coverageThreshold: {
 		global: {
 			branches: 80,
 			functions: 80,
 			lines: 80,
-			statements: 80,
-		},
+			statements: 80
+		}
 	},
 	setupFilesAfterEnv: ['<rootDir>/src/setup.jest.ts'],
 	testPathIgnorePatterns: [
@@ -27,20 +19,20 @@ module.exports = {
 		'<rootDir>/dist/',
 		'<rootDir>/src/main.ts',
 		'<rootDir>/webpack.config.js',
-		'<rootDir>/src/app.config.ts',
+		'<rootDir>/src/app.config.ts'
 	],
 	transform: {
 		'^.+\\.(ts|mjs|js|html)$': [
 			'jest-preset-angular',
 			{
 				tsconfig: '<rootDir>/tsconfig.spec.json',
-				stringifyContentPathRegex: '\\.html$',
-			},
-		],
+				stringifyContentPathRegex: '\\.html$'
+			}
+		]
 	},
 	moduleNameMapper: {
 		'\\.(css|scss)$': 'identity-obj-proxy',
-		'^src/(.*)$': '<rootDir>/src/$1',
+		'^src/(.*)$': '<rootDir>/src/$1'
 	},
-	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)']
 };
