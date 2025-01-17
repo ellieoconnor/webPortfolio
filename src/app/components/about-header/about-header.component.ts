cf6e185bd243072022cf9e726631123f321dddac
Ellie OConnor <ellieoconnor3114@gmail.com>
2025-01-17 18:50:40 -0500

    Enhance About Header with bio, profile image, and styling

--- src/app/components/about-header/about-header.component.ts
+++ src/app/components/about-header/about-header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
 	selector: 'app-about-header',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, NgOptimizedImage],
 	templateUrl: './about-header.component.html',
 	styleUrls: ['./about-header.component.scss']
 })
-export class AboutHeaderComponent {}
+export class AboutHeaderComponent {
+	bio: string = `Hi, I’m Ellie. I’m a front-end developer, avid reader, and eternal optimist. I work at B&L Information Systems, where I use Angular and JavaScript/TypeScript to build ERP solutions that streamline workflows for the metal-casting and foundry industries.`;
+	// todo: Add a link to my resume page on the B&L word.
+}
