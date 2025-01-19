eb5cbdc262f0914ba15f2d3d064f41b951cea73a
Ellie OConnor <ellieoconnor3114@gmail.com>
2025-01-19 17:59:11 -0500

    Added email capability and hide email

--- src/app/components/contact-info/contact-info.component.ts
+++ src/app/components/contact-info/contact-info.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
 import { RouterModule } from '@angular/router';
+import { NgForOf, NgIf } from '@angular/common';
+
+interface ContactItem {
+	label: string;
+	value: string;
+	href?: string;
+}
 
 @Component({
 	selector: 'app-contact-info',
 	standalone: true,
-	imports: [MatDivider, RouterModule],
+	imports: [MatDivider, RouterModule, NgForOf, NgIf],
 	templateUrl: './contact-info.component.html',
 	styleUrl: './contact-info.component.scss'
 })
 export class ContactInfoComponent {
-	contactItems = [
-		{ label: 'Email', value: '<EMAIL>' },
+	emailUser = 'ellieoconnor3114';
+	emailDomain = 'gmail.com';
+
+	contactItems: ContactItem[] = [
+		{ label: 'Email', value: 'Email Me' },
 		{ label: 'GitHub', value: '#' },
 		{ label: 'LinkedIn', value: '#' }
 	];
