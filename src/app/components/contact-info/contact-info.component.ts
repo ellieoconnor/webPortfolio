74272e54f1a726e6c29a18b895b6b18283b43b03
Ellie O'Connor <87724044+ellieoconnor@users.noreply.github.com>
2025-01-24 16:23:46 -0500

    Add Links functionality (#8)
    * Updated navigation links to use dynamic rendering and adjusted spacing for better responsiveness. Refined global and component-specific styles, enhancing consistency and layout scalability across devices. Added minor flexbox improvements and new utility class for better styling reuse.
    
    * Updated navigation links to use dynamic rendering and adjusted spacing for better responsiveness. Refined global and component-specific styles, enhancing consistency and layout scalability across devices. Added minor flexbox improvements and new utility class for better styling reuse.
    
    * Created contact-form component
    
    * Refactor navigation and add routing for Home and Contact pages
    
    Replaced contact-form component with a new ContactInfo component and implemented routing for Home and Contact pages. Updated navigation links to use Angular Router and dynamic routing, enabling navigation between pages. Introduced HomePage component and set up route-based content rendering with router-outlet.
    
    * Added email capability and hide email
    
    * Added links to github, linkedin and email. Adjusted icons with font-awesome
    
    * Use Metropolis font and update related styles
    
    Replaced Inter font with Metropolis across the application by defining it in global styles and updating variables. Adjusted component-specific styles to align with the new typography. Commented out the external Inter font link to avoid conflicts.
    
    * prettier fix
    
    * Refactor testing setup to align with custom jest configuration.
    
    Converted `jest.config.js` to inline Jest configuration in `package.json` and updated related paths and files. Removed obsolete fields in `angular.json` and standardized spectating setup.
    
    * Add unit tests for toggleDarkMode in NavigationLinksComponent
    
    Enhances test coverage by mocking ThemeLogicService and verifying toggleDarkMode behavior. Updates the test setup with RouterTestingModule and mock services to ensure proper isolation and functionality validation.
    
    ---------
    
    Co-authored-by: Elizabeth O'Connor <eoconnor@blinfo.com>

---
+++ src/app/components/contact-info/contact-info.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { MatDivider } from '@angular/material/divider';
+import { RouterModule } from '@angular/router';
+import { NgForOf } from '@angular/common';
+import { FaIconComponent, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+
+interface ContactItem {
+	label: string;
+	icon: IconDefinition;
+	href?: string;
+}
+
+@Component({
+	selector: 'app-contact-info',
+	standalone: true,
+	imports: [MatDivider, RouterModule, NgForOf, FaIconComponent],
+	templateUrl: './contact-info.component.html',
+	styleUrl: './contact-info.component.scss'
+})
+export class ContactInfoComponent {
+	// Email address split for better protection
+	emailUser = 'ellieoconnor3114';
+	emailDomain = 'gmail.com';
+
+	// Font Awesome Icons
+	faEnvelope = faEnvelope;
+	faGitHub = faGithub;
+	faLinkedIn = faLinkedin;
+
+	contactItems: ContactItem[] = [
+		{ label: 'Email', icon: this.faEnvelope, href: `mailto:${this.emailUser}@${this.emailDomain}?Subject=Hello!` },
+		{ label: 'GitHub', icon: this.faGitHub, href: 'https://github.com/ellieoconnor' },
+		{ label: 'LinkedIn', icon: this.faLinkedIn, href: 'https://www.linkedin.com/in/elizabethoconnor13/' }
+	];
+}
--- src/app/components/navigation/navigation-links.component.ts
+++ src/app/components/navigation/navigation-links.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -8,15 +8,32 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { MatButtonModule } from '@angular/material/button';
 import { ThemeLogicService } from '../theme-logic.service';
+import { RouterLink, RouterModule } from '@angular/router';
+
+interface Link {
+	label: string;
+	href: string;
+	target?: string;
+}
 
 @Component({
 	selector: 'app-navigation-links',
 	standalone: true,
-	imports: [CommonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonToggleModule, FaIconComponent, MatButtonModule],
+	imports: [
+		CommonModule,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatListModule,
+		MatButtonToggleModule,
+		FaIconComponent,
+		MatButtonModule,
+		RouterLink,
+		RouterModule
+	],
 	templateUrl: './navigation-links.component.html',
 	styleUrls: ['./navigation-links.component.scss']
 })
-export class NavigationLinksComponent {
+export class NavigationLinksComponent implements OnInit {
 	isDarkMode: boolean = false;
 
 	/* List of Icons */
@@ -25,9 +42,21 @@ export class NavigationLinksComponent {
 		lightModeSun: faSun
 	};
 
+	links: Link[] = [
+		{ label: 'Home', href: '/home' },
+		{ label: 'Blog', href: '/blog' },
+		{ label: 'Contact', href: '/contact' }
+	];
+
 	constructor(private themeService: ThemeLogicService) {}
+
+	ngOnInit(): void {
+		this.isDarkMode = this.themeService.darkModeEnabled;
+	}
 
 	toggleDarkMode() {
 		this.themeService.toggleTheme();
+
+		this.isDarkMode = this.themeService.darkModeEnabled;
 	}
 }
