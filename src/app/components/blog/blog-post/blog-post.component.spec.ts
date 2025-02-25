567df9feaac825dd4124845dc8d50b004dfb9ecd
Ellie OConnor <ellieoconnor3114@gmail.com>
2025-02-24 19:52:09 -0500

    "Refactor test files to use consistent indentation style

--- src/app/components/blog/blog-post/blog-post.component.spec.ts
+++ src/app/components/blog/blog-post/blog-post.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BlogPostComponent } from './blog-post.component';
 
 describe('BlogPostComponent', () => {
-  let component: BlogPostComponent;
-  let fixture: ComponentFixture<BlogPostComponent>;
+	let component: BlogPostComponent;
+	let fixture: ComponentFixture<BlogPostComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BlogPostComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [BlogPostComponent]
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(BlogPostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(BlogPostComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
