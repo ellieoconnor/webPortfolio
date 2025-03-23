347d16d5d9d61a6035c30a355f7ace0398f40d74
ellieoconnor <ellieoconnor3114@gmail.com>
2025-03-23 12:22:58 -0400

    prettier check

--- src/app/components/project-list/project-list.component.spec.ts
+++ src/app/components/project-list/project-list.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProjectListComponent } from './project-list.component';
 
 describe('ProjectListComponent', () => {
-  let component: ProjectListComponent;
-  let fixture: ComponentFixture<ProjectListComponent>;
+	let component: ProjectListComponent;
+	let fixture: ComponentFixture<ProjectListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProjectListComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ProjectListComponent]
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(ProjectListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(ProjectListComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-project-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './project-list.component.html',
-  styleUrl: './project-list.component.scss'
+	selector: 'app-project-list',
+	standalone: true,
+	imports: [],
+	templateUrl: './project-list.component.html',
+	styleUrl: './project-list.component.scss'
 })
-export class ProjectListComponent {
-
-}
+export class ProjectListComponent {}
