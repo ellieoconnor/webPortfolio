8dd524d013eeba468293c1e888252dcef9967e49
ellieoconnor <ellieoconnor3114@gmail.com>
2025-04-21 15:29:02 -0400

    initial commit; worked on home page responsive size.

--- src/app/components/navigation/navigation-links.component.ts
+++ src/app/components/navigation/navigation-links.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { faHamburger, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import { MatButtonModule } from '@angular/material/button';
 import { ThemeLogicService } from '../theme-logic.service';
 import { RouterLink, RouterModule } from '@angular/router';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 interface Link {
 	label: string;
@@ -28,18 +30,24 @@ interface Link {
 		FaIconComponent,
 		MatButtonModule,
 		RouterLink,
-		RouterModule
+		RouterModule,
+		MatMenuTrigger,
+		MatMenu,
+		MatSlideToggle
 	],
 	templateUrl: './navigation-links.component.html',
 	styleUrls: ['./navigation-links.component.scss']
 })
 export class NavigationLinksComponent implements OnInit {
 	isDarkMode: boolean = false;
+	isMobileMenuOpen = false;
+	isMobileView = false;
 
 	/* List of Icons */
 	icons = {
 		darkModeMoon: faMoon,
-		lightModeSun: faSun
+		lightModeSun: faSun,
+		menu: faHamburger
 	};
 
 	links: Link[] = [
@@ -49,10 +57,22 @@ export class NavigationLinksComponent implements OnInit {
 		{ label: 'My Work', href: '/my-work', target: '_self' }
 	];
 
-	constructor(private themeService: ThemeLogicService) {}
+	constructor(private themeService: ThemeLogicService) {
+		this.checkScreenSize();
+	}
 
 	ngOnInit(): void {
 		this.isDarkMode = this.themeService.darkModeEnabled;
+	}
+
+	@HostListener('window:resize')
+	checkScreenSize() {
+		this.isMobileView = window.innerWidth < 768;
+
+		// Close mobile menu if screen is resized to desktop view
+		if (!this.isMobileView && this.isMobileMenuOpen) {
+			this.isMobileMenuOpen = false;
+		}
 	}
 
 	toggleDarkMode() {
