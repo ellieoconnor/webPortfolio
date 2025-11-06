caefe7fc9efe6ddc8acb3179e7fba9b871916f70
Ellie O'Connor <87724044+ellieoconnor@users.noreply.github.com>
2025-11-05 19:40:56 -0500

    Refactor/bio text change (#17)
    * initial commit; worked on home page responsive size.
    
    * ```
    Refactor navigation styling and theme toggle behavior
    
    Updated navigation links to use a column layout for better responsiveness. Added new styles for theme toggle items and replaced the hamburger menu icon with a bars icon. Adjusted theme toggle to prevent propagation of click events.
    ```
    
    * `Adjust navigation styles for better responsiveness`
    
    * refator: Update bio content and improve styling in AboutHeader component
    
    * feat: Implements project listing with dynamic content and styling
    
    Added a grid layout for displaying projects dynamically using Angular Material components. Introduced `ProjectInfo` interface and a model for managing project data. Updated the template and styles to include cards with tech stack chips, links to GitHub, and live demos.

--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ProjectInfo, PROJECTS } from './project.model';
+import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { MatChip, MatChipSet } from '@angular/material/chips';
 
 @Component({
 	selector: 'app-project-list',
 	standalone: true,
-	imports: [],
+	imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatChipSet, MatChip, MatCardActions],
 	templateUrl: './project-list.component.html',
 	styleUrl: './project-list.component.scss'
 })
-export class ProjectListComponent {}
+export class ProjectListComponent {
+	projectList: ProjectInfo[] = PROJECTS;
+}
