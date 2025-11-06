9bf1780c1a12b5a89e3b1033efd7623f2895aece
ellieoconnor <ellieoconnor3114@gmail.com>
2025-11-05 19:33:08 -0500

    feat: Implements project listing with dynamic content and styling
    Added a grid layout for displaying projects dynamically using Angular Material components. Introduced `ProjectInfo` interface and a model for managing project data. Updated the template and styles to include cards with tech stack chips, links to GitHub, and live demos.

---
+++ src/app/components/project-list/project.model.ts
@@ -0,0 +1,18 @@
+export interface ProjectInfo {
+	title: string;
+	description: string;
+	image?: string;
+	techUsed: string[];
+	githubUrl: string;
+	liveUrl?: string;
+}
+
+export const PROJECTS: ProjectInfo[] = [
+	{
+		title: 'Legend of Zelda - Breath of the Wild Compendium',
+		description:
+			'A lookup tool for creatures, weapons, monsters and treasures from Breath of the Wild. Users can search items or use categories to explore. Built using the Hyrule Compendium API.',
+		techUsed: ['JavaScript', 'HTML', 'CSS'],
+		githubUrl: 'https://github.com/ellieoconnor/hyrule-compendium.git'
+	}
+];
