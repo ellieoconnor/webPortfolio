5c0ed1ee0771cbe5381e38e611714f081fa54b75
Ellie O'Connor <87724044+ellieoconnor@users.noreply.github.com>
2025-11-16 20:40:05 -0500

    fix: button alignments (#20)

--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ProjectInfo, PROJECTS } from './project.model';
 import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatChip, MatChipSet } from '@angular/material/chips';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
 	selector: 'app-project-list',
 	standalone: true,
-	imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatChipSet, MatChip, MatCardActions],
+	imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatChipSet, MatChip, MatCardActions, MatAnchor],
 	templateUrl: './project-list.component.html',
 	styleUrl: './project-list.component.scss'
 })
