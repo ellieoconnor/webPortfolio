f8c7011e04978c816ec49e77d1344f7348f53919
ellieoconnor <ellieoconnor3114@gmail.com>
2026-01-14 05:28:41 -0500

    project-list component: improved styles and added new project entry

--- src/app/components/project-list/project.model.ts
+++ src/app/components/project-list/project.model.ts
@@ -3,7 +3,7 @@ export interface ProjectInfo {
 	description: string;
 	image?: string;
 	techUsed: string[];
-	githubUrl: string;
+	githubUrl?: string;
 	liveUrl?: string;
 }
 
@@ -15,5 +15,12 @@ export const PROJECTS: ProjectInfo[] = [
 		techUsed: ['JavaScript', 'HTML', 'CSS'],
 		githubUrl: 'https://github.com/ellieoconnor/hyrule-compendium.git',
 		liveUrl: 'https://hyrule-botw-compendium.netlify.app/'
+	},
+	{
+		title: 'Neat Nest Cleaning Services',
+		description:
+			'A cleaning service for homes and businesses. Offering a range of services including deep cleaning, carpet cleaning and window washing.',
+		techUsed: ['JavaScript', 'HTML', 'CSS'],
+		liveUrl: 'https://neat-nest-cleaning.netlify.app/'
 	}
 ];
