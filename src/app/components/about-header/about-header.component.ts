c683b78dd4e5123457a99ec0f85de54972e17931
Ellie O'Connor <87724044+ellieoconnor@users.noreply.github.com>
2026-01-22 06:04:06 -0500

    Fix/refinements (#23)
    * new post: "Legend Of Imposters: How a Beginner Project Helped Me Battle Shame" added to posts and content
    
    * minor corrections to bio text in about-header component
    
    * project-list component: improved styles and added new project entry
    
    * project-list component: added MatButton to imports

--- src/app/components/about-header/about-header.component.ts
+++ src/app/components/about-header/about-header.component.ts
@@ -11,11 +11,11 @@ export class AboutHeaderComponent {
 	// bio: string = `Hi, I'm Ellie, and I am a full-stack software developer. At B&L Information Systems, I create digital solutions with Angular and JavaScript/TypeScript that transform complex challenges into seamless experiences. I enjoy bringing ideas to life through code and am always eager to help others solve their digital challenges. Interested in exploring how technology can solve your unique challenges?
 	bio: string = `I love the process.
 
-	Whether I'm debugging a tricky issue, designing a new feature, or building something from scratch, I find joy in the thinking, tinkering, and refining. I love coming up wiht ideas, testing theories, and slowlying shaping them into the exact right solution. The best moments? When the result is so smooth, the user never even notices the complexity underneath. That's when I know I've done it right.
+	Whether I'm debugging a tricky issue, designing a new feature, or building something from scratch, I find joy in the thinking, tinkering, and refining. I love coming up with ideas, testing theories, and slowly shaping them into the exact right solution. The best moments? When the result is so smooth, the user never even notices the complexity underneath. That's when I know I've done it right.
 
 	I'm Elizabeth (but you can call me Ellie), a software engineer currently working on ERP solutions tailored for the metal casting industry. I care deeply about solving real problems that make people's lives easier, and I am honoured that I get to do so with code.
 
-	Outside of work, you'll usually find me playing guitar, lifting weights, being way too competitive in a board game with loved ones, or getting lost in my latest of many hobbies (currently customizing mechanical keyboards). And yeah, as cringy as it might sound--I really love coding.
+	Outside of work, you'll usually find me playing guitar, lifting weights, being way too competitive in a board game with loved ones, or getting lost in my latest of many hobbies (currently customizing mechanical keyboards). And yeah, as cringy as it might sound – I really love coding.
 
 	Get in touch and let's enjoy the process of building something together.`;
 	// todo: Add a link to my resume page on the B&L word.
--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { ProjectInfo, PROJECTS } from './project.model';
 import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatChip, MatChipSet } from '@angular/material/chips';
-import { MatAnchor } from '@angular/material/button';
+import { MatAnchor, MatButton } from '@angular/material/button';
 
 @Component({
 	selector: 'app-project-list',
 	standalone: true,
-	imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatChipSet, MatChip, MatCardActions, MatAnchor],
+	imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatChipSet, MatChip, MatCardActions, MatAnchor, MatButton],
 	templateUrl: './project-list.component.html',
 	styleUrl: './project-list.component.scss'
 })
